1ed54d23917a1609d809648d3194113c4a4c9247
Lachicagradiadora <quintanilla.roseny@gmail.com>
2023-03-08 23:02:55 -0500

    variables in javascript

---
+++ variables.js
@@ -0,0 +1,27 @@
+// VARIABLES
+
+// var: representacion del lugar donde guardaremos valores
+var nombre = "Oscar";
+//mira, una variable: tengo que reservar un espacio en mi memoria para guardar un valor
+// DOS estados de una variable
+// 1. DECLARAR
+var edad;
+// INICIALIZAR
+edad = 30
+
+edad
+
+// varriables del tipo array: conjunto de elementos
+var elementos = ["computadora","celular"];
+// =: para asignar un valor
+
+// variable tipo objeto
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+// variable compleja porque guarda mas valores de diferentes tipos
+
+// llamo a la variable "persona", asi llamamos a todo lo que tiene que ver con ella
+persona;
+
