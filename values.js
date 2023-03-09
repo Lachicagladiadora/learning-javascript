7de0b9d08d34c34e5c7c942ee64c07f092aadbae
Lachicagradiadora <quintanilla.roseny@gmail.com>
2023-03-08 22:56:58 -0500

    valores en javascript

---
+++ values.js
@@ -0,0 +1,24 @@
+// VALORES (DATOS)
+
+// Tipos
+
+//1. numero
+40
+
+//2. string: texto, siempre entre ""
+"Lachicagladiadora"
+
+//3. boolean: validar cosas; verdadero o falso
+true 
+false 
+
+//4. (valores vacios): valores (place holder) valores reservados
+null
+undefined 
+// tambien pueden ser producto de un error
+
+//5. no primitivos (tipo OBJETO)
+// tipo "array"
+[1,2,3]
+// tipo "objeto"
+{nombre: Roberto}
