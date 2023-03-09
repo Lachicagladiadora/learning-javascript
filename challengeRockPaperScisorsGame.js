24db21238ed54df45aec811faa1cff64c3dc8b2b
Lachicagradiadora <quintanilla.roseny@gmail.com>
2023-03-09 17:02:29 -0500

    Challenge of game Rock, Paper and Scizor with "if" and  "switch" Se solucionaron los problemas del codigo con -if- Se hizo el reto con -switch- Los codigos funcionan muy bien, no hay errores

--- challengeRockPaperScisorsGame.js
+++ challengeRockPaperScisorsGame.js
@@ -21,47 +21,6 @@ function play(player,computer){
 
 play("rock","scifzor")
 
-// codigo 2
-// variables
-var paper = "paper"
-var rock = "rock"
-var scisor = "scisor"
-// function
-function play (you,computer){
-    // dead heat
-    if (paper === "paper") console.log ("dead heat")
-    else if (rock === "rock") console.log ("dead heat")
-    else if (scisor === "scisor") console.log ("dead heat")    
-    // cumputer win
-    else if (rock === "paper") console.log ("you lost, computer win")
-    else if (paper === "scisor") console.log ("you lost, computer win")
-    else if (scisor === "rock") console.log ("you lost, computer win")
-    // you win
-    else if (paper === "rock") console.log ("congratulation, you win")
-    else if (scisor === "paper") console.log ("congratulation you win")
-    else  console.log ("congratulation you win")
-}
-// invoke function
-game(you,computer)
-
-
-// codigo 3
-// var paper = "paper"
-// var rock = "rock"
-// var scisor = "scisor"
-// function play (you, computer) {
-//     you = paper === paper ? "Dead heat" : "Intentalo otra vez"
-//     you = paper === rock ? "You win" : "Intentalo otra vez"
-//     you = paper === scisor ? "You lost" : "Intentalo otra vez"
-//     you = rock === rock ? "Dead heat" : "Intentalo otra vez"
-//     you = rock === scisor ? "You win" : "Intentalo otra vez"
-//     you = rock === paper ? "You lost" : "Intentalo otra vez"
-//     you = scisor === scisor ? "Dead heat" : "Intentalo otra vez"
-//     you = scisor === rock ? "You win" : "Intentalo otra vez"
-//     you = scisor === paper ? "You lost" : "Intentalo otra vez"
-// }
-// play (you, computer)
-
 
 // codigo 4
 var paper = "paper"
@@ -70,43 +29,67 @@ var scisor = "scisor"
 function play (you, computer) {
     // you !== computer
     if (you !== computer) {
-        if (rock !== paper) console.log ("you lost, computer win")
-        else if (paper == rock)  console.log ("congratulations, you win")
+        if (you===rock && computer===paper) console.log ("You lost, computer win")
+        else if (you===paper && computer===rock)  console.log ("Congratulations, you win")
 
-        else if (paper !== scisor) console.log ("you lost, computer win")
-        else if (scisor == paper) console.log ("congratulations, you win")
+        else if (you===paper && computer===scisor) console.log ("You lost, computer win")
+        else if (you===scisor && computer===paper) console.log ("Congratulations, you win")
 
-        else if (scisor !== rock) console.log ("you lost, computer win")
-        else console.log ("congratulations, you win")
+        else if (you===scisor && computer===rock) console.log ("You lost, computer win")
+        else console.log ("Congratulations, you win")
     // you && computer
     } else {
         console.log("Dead heat, intentalo otra vez")
     }
 }
 play (you, computer)
-// reconoce el empate y cuando no es empate, falta que reconozca la diferncia de orden de paper-rock y rock-paper
 
 
 
-// Aporte de otro estudiante
-var op1 = "Piedra";
-var op2 = "Papel";
-var op3 = "Tijera";
 
-var resultado = function(user, cpu){
-    if(user != cpu){
-        if(user === op1 && cpu === op3){
-            console.log("el usuario GANO con "+ op1)
-        }else if(user === op2 && cpu === op1){
-            console.log( "el usuario GANO con " + op2)
-        }else if(user === op3 && cpu === op2){
-            console.log("el usuario GANO con " + op3)
-        }else{
-            console.log("La CPU Gano!!")
-        }
-    }else if(user === cpu){
-        console.log("Empate")
+
+
+// codigo del juego con switch
+var paper = "paper"
+var rock = "rock"
+var scisor = "scisor"
+function play (you, computer) {
+    
+    switch (true) {
+        // you && computer    
+        case (you === computer):
+            console.log("Dead heat, intentalo de nuevo")
+            break;
+        // you !== computer    
+        case (rock && paper):
+            console.log("You lost, computer win")
+            break;
+        case (you===paper && computer===rock):
+            console.log("Congratulations, you win")
+            break;
+        case (you===paper && computer===scisor):
+            console.log("You lost, computer win")
+            break;
+        case (you===scisor && computer===paper):
+            console.log("Congratulations, you win")
+            break;
+        case (you===scisor && computer===rock):
+            console.log("You lost, computer win")
+            break;
+        case (you===rock && computer===scisor):
+            console.log("Congratulations, you win")
+            break;
+
+        default:
+            console.log("ocurrio un error, intentalo otra vez");
+            break;
     }
-};
+}
+play (you, computer)
 
-resultado(op1,op3) //el usuario GANO con Piedra```
+
+
+
+
+
+
