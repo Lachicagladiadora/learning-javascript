42f47eafc0b17e615b1aa36b78c38c13563d2036
Lachicagradiadora <quintanilla.roseny@gmail.com>
2023-03-08 23:05:05 -0500

    functions and types

---
+++ functions.js
@@ -0,0 +1,20 @@
+// FUNCIONES
+
+// Dos tipos de funciones
+// 1. Function: DECLARATIVA
+function miFuncion() {
+    return 3;
+}
+miFuncion();
+
+
+// 2. Function: DE EXPRESION
+// hay una funcion dentro de la variable, 
+var miFuncion = function(a,b) {
+    return a + b;
+}
+
+// Una funcion necesita parametros que seran utilizados para trabajar con las funciones
+
+// mandamos llamar la funcion declarativa
+miFuncion();
