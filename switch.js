b1eb8891ab4c25d48fa484795551546ab9983f43
Lachicagradiadora <quintanilla.roseny@gmail.com>
2023-03-09 22:21:08 -0500

    Switch, para que sive y su sintaxis

---
+++ switch.js
@@ -0,0 +1,20 @@
+// SWITCH
+// sirve para validar "casos" mediante una condicion
+// es diferente dol "if"
+
+var numero = 1
+switch (numero) { 
+    case 1: //pueden haber muchos casos
+        console.log("soy uno");
+        break; //rompe la validadion y solo imprime lo devido
+    case 10: 
+        console.log("Soy un 10");
+        break;
+    case 100: 
+        console.log("Soy un 100");
+        break;
+    default: // si no cumple con los casos mencionados, se valida el default
+        console.log("No soy nada");
+}
+
+// si no colocamos el "break", imprime todo cumpliendose o no el caso
