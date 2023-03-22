45b252f57c0a1b726b7592d8df2dd4fb425c3038
Lachicagradiadora <200379@unsaac.edu.pe>
2023-03-22 12:53:16 -0500

    area of triangle and square

--- basicFormulas.js
+++ basicFormulas.js
@@ -2,6 +2,7 @@
 
 // perimeter y area de un quadrilateral 
 // quadrilateral regular
+console.group('Square');
 const squareSide = 5;
 const squarePerimeter = squareSide * 4;
 const squareArea = squareSide * squareSide;
@@ -12,7 +13,16 @@ console.log ({
     squareArea,
 });
 
+function calculateSquare(squareSide) {
+    return {
+        squarePerimeter: squareSide * 4,
+        squareArea: squareSide * squareSide
+    };
+}
+console.groupEnd('Square');
+
 // triangle regular 
+console.group('Triangle');
 const triangleSide = 6;
 const triangleHeight = ((triangleSide/2) *(3^(1/2)));
 const trianglePerimeter = triangleSide * 3;
@@ -24,3 +34,11 @@ console.log ({
     trianglePerimeter,
     triangleArea, 
 })
+
+function calculateTriangle(triangleSide1, triangleSide2, triangleBase, triangleHeight) {
+    return {
+        trianglePerimeter: triangleSide1 + triangleSide2 + triangleBase,
+        trianglePerimeter: (triangleBase * triangleHeight) / 2,
+    };
+}
+console.groupEnd('Triangle');
