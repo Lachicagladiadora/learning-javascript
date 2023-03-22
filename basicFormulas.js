9ad2af60e1e3032305e2a0928171ec312f7e21fe
Lachicagradiadora <200379@unsaac.edu.pe>
2023-03-21 23:54:16 -0500

    area and perimeter of the square and equilateral triangle

---
+++ basicFormulas.js
@@ -0,0 +1,26 @@
+// Basic Formulas
+
+// perimeter y area de un quadrilateral 
+// quadrilateral regular
+const squareSide = 5;
+const squarePerimeter = squareSide * 4;
+const squareArea = squareSide * squareSide;
+
+console.log ({
+    squareSide,
+    squarePerimeter,
+    squareArea,
+});
+
+// triangle regular 
+const triangleSide = 6;
+const triangleHeight = ((triangleSide/2) *(3^(1/2)));
+const trianglePerimeter = triangleSide * 3;
+const triangleArea = (triangleSide * triangleHeight)/2;
+
+console.log ({
+    triangleSide,
+    triangleHeight,
+    trianglePerimeter,
+    triangleArea, 
+})
