6d006db15d4eca6d19d9cbe008e15e0cacf1036e
Lachicagradiadora <200379@unsaac.edu.pe>
2023-03-23 22:20:23 -0500

    reassignment and redecalration of variables

---
+++ reassignmentAnd RedecalrationOfVariables.js
@@ -0,0 +1,36 @@
+// Reassignment and Redecalration of Variables
+
+//var
+var firstName;//Undefined
+firstName='Oscar';
+console.log(firstName);
+// Reasignacion
+var lastName='David';//declarar / asignar
+lastName='Ana';//reasignar
+console.log(lastName);
+// Reasignacion
+var secondName='Ricardo';//declarando / asignando
+var secondName='Jose';//reasignando
+console.log(secondName);
+
+
+//let
+let fruit='Apple';//declara y asigna
+fruit='Kiwi';//reasignar
+console.log(fruit);
+
+let fruit='Banana';
+console.log(fruit);
+
+
+//const
+const animal='dog';//declara y asigna
+animal='cat';//reasignando
+console.log(animal);
+
+const vehicles=[];
+vehicles.push('door');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
