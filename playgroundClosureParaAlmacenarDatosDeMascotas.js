917df516a6b59ac82383e7c6352fe1aebe633e13
Lachicagradiadora <200379@unsaac.edu.pe>
2023-03-24 19:38:12 -0500

    playground 2: closure para almacenar datos de mascotas

---
+++ playgroundClosureParaAlmacenarDatosDeMascotas.js
@@ -0,0 +1,35 @@
+// Playground: Closure para Almacenar Datos de Mascotas
+
+// guia
+En este desafío tendrás que crear un closure que nos permita almacenar datos de mascotas en cualquier momento.
+Los datos pueden venir de distintas maneras, pueden ser objetos, strings o arrays. En el primer llamado a la función nos servirá para crear nuestra lista en un inicio mientras que los demás llamados nos ayudará a agregar elementos al final de la lista.
+Si en algún momento llamamos a la función sin pasarle ningún parámetro esta nos devolverá el array con los datos de todos las mascotas introducidas.
+
+Input:
+const myPetList = createPetList();
+myPetList("michi");
+myPetList("firulais");
+myPetList();
+
+Output:
+["michi", "firulais"]
+
+// codigo
+export function createPetList() {
+    // Tu código aquí 👈
+    const petList = [];
+    function myPetList(newPet) {
+      if (Boolean(newPet) === true) {
+        petList.push(newPet);
+        // console.log(petList);
+      } else {
+        console.log(petList);
+      }
+    }
+    return myPetList;
+  }
+  
+  // const myPetList = createPetList();
+  // myPetList("michi");
+  // myPetList("firulais");
+  // myPetList();
