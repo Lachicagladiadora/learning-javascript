e6b0bc95d77675b7b4c23bf40f76b2c75206d9e7
Lachicagradiadora <200379@unsaac.edu.pe>
2023-04-03 17:18:43 -0500

    correct codeto playground almacenar datos de mascotas

--- playgroundClosureParaAlmacenarDatosDeMascotas.js
+++ playgroundClosureParaAlmacenarDatosDeMascotas.js
@@ -1,7 +1,6 @@
 // Playground: Closure para Almacenar Datos de Mascotas
 
 // guia
-<<<<<<< HEAD
 // En este desafío tendrás que crear un closure que nos permita almacenar datos de mascotas en cualquier momento.
 // Los datos pueden venir de distintas maneras, pueden ser objetos, strings o arrays. En el primer llamado a la función nos servirá para crear nuestra lista en un inicio mientras que los demás llamados nos ayudará a agregar elementos al final de la lista.
 // Si en algún momento llamamos a la función sin pasarle ningún parámetro esta nos devolverá el array con los datos de todos las mascotas introducidas.
@@ -11,37 +10,32 @@
 // myPetList("michi");
 // myPetList("firulais");
 // myPetList();
-=======
-En este desafío tendrás que crear un closure que nos permita almacenar datos de mascotas en cualquier momento.
+
+/*En este desafío tendrás que crear un closure que nos permita almacenar datos de mascotas en cualquier momento.
 Los datos pueden venir de distintas maneras, pueden ser objetos, strings o arrays. En el primer llamado a la función nos servirá para crear nuestra lista en un inicio mientras que los demás llamados nos ayudará a agregar elementos al final de la lista.
 Si en algún momento llamamos a la función sin pasarle ningún parámetro esta nos devolverá el array con los datos de todos las mascotas introducidas.
-
-Input:
+*/
+// Input:
 const myPetList = createPetList();
 myPetList("michi");
 myPetList("firulais");
 myPetList();
->>>>>>> refs/remotes/origin/main
 
-Output:
+
+// Output:
 ["michi", "firulais"]
 
 // codigo
 export function createPetList() {
     // Tu código aquí 👈
     const petList = [];
-    function myPetList(newPet) {
-      if (Boolean(newPet) === true) {
-        petList.push(newPet);
-        // console.log(petList);
-      } else {
-        console.log(petList);
-      }
+  function myPetList(newPet) {
+    if (Boolean(newPet) === true) {
+      return petList.push(newPet);
+    } else {
+      return petList
     }
-    return myPetList;
+  }
+  return myPetList;
   }
   
-  // const myPetList = createPetList();
-  // myPetList("michi");
-  // myPetList("firulais");
-  // myPetList();
