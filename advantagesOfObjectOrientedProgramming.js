94b45f2e2c08311bf3692af363e7a2542514437a
Lachicagradiadora <200379@unsaac.edu.pe>
2023-04-05 20:11:11 -0500

    add some class(prototype), get and set

--- advantagesOfObjectOrientedProgramming.js
+++ advantagesOfObjectOrientedProgramming.js
@@ -68,7 +68,63 @@ const fito = {
 //Programacion Orientada a Objetos
 //Crearemos instancias a partir del prototype
 
-class learningPath{
+class Classes{
+    constructor({
+        name,
+        description,
+        teacher
+    }){
+        this.name=name;
+        this.description=description;
+        this.teacher=teacher
+    }
+}
+
+class Course{
+    constructor({
+        name,
+        classes=[],
+        comments=[],
+        resources=[]
+    }) {
+        this._name=name;
+        this.classes=classes;
+        this.comments=comments;
+        this.resources=resources
+    }
+    get name(){
+        return this._name
+    }
+    // changeName(newName){
+    //     this._name= newName
+    // }
+    set name(newName){
+        if(newName==='Curso Maldito') console.error('website no found')
+        else this._name=newName
+    }
+}
+
+const basicProgrammingCourse=new Course ({
+    name:'Free Basic Programming Course',
+})
+// Course.basicProgrammingCourse.name //Free Basic Programming Course
+// Course.basicProgrammingCourse.name='new name' //new name
+// Course.basicProgrammingCourse.changeName('new name') //new name
+
+const htmlCourse=new Course ({
+    name:'Ultimate HTML Course',
+})
+const htmlPractice=new Course ({
+    name:'HTML Practice Course',
+})
+const cssCourse=new Course ({
+    name:'Ultimate CSS Course',
+})
+const cssPractice=new Course ({
+    name:'CSS Practice Course',
+})
+
+class LearningPath{
     constructor({
         name,
         courses=[],
@@ -77,26 +133,31 @@ class learningPath{
         this.course=courses;
     }
 }
-const schoolWeb=new learningPath({
+const schoolWeb=new LearningPath({
     name:'School Web',
     courses:[
-        'HTML',
-        'CSS',
-        'Java Script',
+        basicProgrammingCourse,
+        htmlCourse,
+        htmlPractice,
+        cssCourse,
+        cssPractice
     ]
 })
-const schoolData=new learningPath({
+const schoolData=new LearningPath({
     name: 'School Data Science',
     courses: [
+        'Basic Programming',
         'Data Analytics',
-        'cfvgbhunj'
+        'cfvgbhunj',
     ]
 })
-const schoolVgs=new learningPath({
+const schoolVgs=new LearningPath({
     name:'School Videogames',
     courses: [
+        'Basic Programming',
         'Introduction of videogames',
-        'fdrvgthjuybvfdcs'
+        'Unity course', 
+        'Unreal course'
     ]
 })
 
