caf463aac4376109d34bb660b4cf3cbce15226e8
Lachicagradiadora <200379@unsaac.edu.pe>
2023-04-27 19:06:38 -0500

    learning to validate entered values

---
+++ forms/form1.js
@@ -0,0 +1,72 @@
+//seleccionamos los elementos del html
+const form = document.getElementById('form')
+const buttonSubmit = document.getElementById('submit-button')
+
+const name = document.getElementById('name-input')
+const lastName = document.getElementById('last-name-input')
+const age = document.getElementById('age-input')
+const email = document.getElementById('email-input')
+const password = document.getElementById('password-input')
+const passwordConfirmation = document.getElementById('password-confirmation-input')
+
+// validamos
+if (!form) console.warn('the id of "formElement", was not found')
+if (!buttonSubmit) console.warn('the id of "buttonSubmit", was not found')
+
+if (!name) console.warn('the id of "nameElement", was not found')
+if (!lastName) console.warn('the id of "lastNameElement", was not found')
+if (!age) console.warn('the id of "ageElement", was not found')
+if (!email) console.warn('the id of "emailElement", was not found')
+if (!password) console.warn('the id of "passwordElement", was not found')
+if (!passwordConfirmation) console.warn ('the id of "passwordConfirmationElement", was not found')
+
+// FUNCTIONS
+const submitDatesValid = {
+    name:false,
+    lastName:false,
+    age:false,
+    email:false,
+    password:false,
+    passwordConfirmation:false
+}
+console.log('1')
+// EVENTS
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
+
+name.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) submitDatesValid.name = true
+})
+
+lastName.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) submitDatesValid.lastName = true
+})
+
+age.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) submitDatesValid.age = true
+})
+
+email.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) submitDatesValid.email = true
+})
+
+password.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) submitDatesValid.password = true
+})
+
+passwordConfirmation.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) submitDatesValid.passwordConfirmation = true
+})
+console.log('2')
+const validateForm = () => {
+    const formDatesValid = Object.values(submitDatesValid)
+    const validate = formDatesValid.findIndex(value => value == false)
+    console.log('3')
+    if(validate == -1) form.submit()
+    else alert ('form invalid')
+}
+
+buttonSubmit.addEventListener('click', validateForm)
+
+console.log('4')
