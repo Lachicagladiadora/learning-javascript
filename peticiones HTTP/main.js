d78bb22e9c30918d66f0fe92207b14e37f95f818
Lachicagradiadora <200379@unsaac.edu.pe>
2023-04-28 23:55:50 -0500

    add other http requests

--- peticiones HTTP/main.js
+++ peticiones HTTP/main.js
@@ -2,23 +2,36 @@
 console.log('Hello Chuck Norris')
 
 const textElement = document.getElementById("phrase")
+const imgElement = document.getElementById('imgNasa')
 
-const URL = 'https://api.chucknorris.io/jokes/random'
-
-// fetch(URL)
-//     .then(res => res.json())
-//     .then(data => {
-//         const textElement = document.getElementById("phrase")
-//         textElement.innerText=data[6].value  
-//     })
-
+const chucknorris = 'https://api.chucknorris.io/jokes/random'
+const imgNasa= 'https://images-api.nasa.gov'
 
 //solicitud GET utilizando la API fetch
 
-fetch(URL)
+fetch(chucknorris)
 .then((response) => response.json())
 .then((json) => {
     console.log(json)
     
     textElement.innerText=json.value
 });
+
+
+//dolicitud 
+const chargeImgNasa = async() => {
+    try{
+        const answer = await fetch(imgNasa)
+        console.log(answer)
+        .then((resp) => resp.json())
+        .then((json) => {
+            
+            imgElement.src=json[8].url
+        })
+        
+    } catch (error) {
+        console.log('error')
+    }
+}
+
+chargeImgNasa()
