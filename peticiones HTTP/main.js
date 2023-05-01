46ae61859498169c6e5660e2e6ef8598fa862fd3
Lachicagradiadora <200379@unsaac.edu.pe>
2023-04-30 23:39:35 -0500

    add styles and event click

--- peticiones HTTP/main.js
+++ peticiones HTTP/main.js
@@ -2,10 +2,9 @@
 console.log('Hello Chuck Norris')
 
 const textElement = document.getElementById("phrase")
-const imgElement = document.getElementById('imgNasa')
+const buttonImg = document.getElementById("chuck-norris")
 
 const chucknorris = 'https://api.chucknorris.io/jokes/random'
-const imgNasa= 'https://images-api.nasa.gov'
 
 //solicitud GET utilizando la API fetch
 
@@ -18,20 +17,8 @@ fetch(chucknorris)
 });
 
 
-//dolicitud 
-const chargeImgNasa = async() => {
-    try{
-        const answer = await fetch(imgNasa)
-        console.log(answer)
-        .then((resp) => resp.json())
-        .then((json) => {
-            
-            imgElement.src=json[8].url
-        })
-        
-    } catch (error) {
-        console.log('error')
-    }
+const reload = () => {
+    location.reload()
 }
 
-chargeImgNasa()
+buttonImg.addEventListener('click' , reload)
