3d674590b3ed1c09ceec9c6a5654008a3f641f12
Lachicagradiadora <200379@unsaac.edu.pe>
2023-05-01 09:07:42 -0500

    message update with enter and space

--- peticiones HTTP/main.js
+++ peticiones HTTP/main.js
@@ -1,6 +1,7 @@
 
 console.log('Hello Chuck Norris')
 
+const bodyElement = document.getElementById("body")
 const textElement = document.getElementById("phrase")
 const buttonImg = document.getElementById("chuck-norris")
 
@@ -21,4 +22,22 @@ const reload = () => {
     location.reload()
 }
 
+const messageEnter = (event) => {
+    if (event.keyCode == 13 || event.key === "Enter") {
+      event.preventDefault()
+      buttonImg.click()
+    }
+    reload
+}
+
+const messageSpace = (e) => {
+    if (e.keyCode == 32 || e.key === "Space") {
+        e.preventDefault()
+        buttonImg.click()
+      }
+      reload
+}
+
 buttonImg.addEventListener('click' , reload)
+bodyElement.addEventListener('keyup', messageEnter)
+bodyElement.addEventListener('keyup', messageSpace)
